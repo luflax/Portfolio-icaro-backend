68999aa0452d6e9fbd6d29c458244b3d21fbb155
Luiz Flavio <luflax34@gmail.com>
2019-08-23 15:36:32 -0300

    fix db

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/picosite',
+module.exports = mongoose.connect('mongodb+srv://admin:admin@picocluster-h68ia.mongodb.net/test?retryWrites=true&w=majority',//'mongodb://localhost/picosite',//
 {
     useNewUrlParser: true
 })
